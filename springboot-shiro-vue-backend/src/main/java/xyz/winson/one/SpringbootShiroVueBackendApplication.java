cb422a1a26ff3ca0889e5c7f66acd048a38e19d5
wenwc <wenwc@xiaopeng.com>
2019-09-25 19:46:07 +0800

    提交后端初始化代码

---
+++ springboot-shiro-vue-backend/src/main/java/xyz/winson/one/SpringbootShiroVueBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.winson.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShiroVueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootShiroVueBackendApplication.class, args);
+	}
+
+}
---
+++ springboot-shiro-vue-backend/src/test/java/xyz/winson/one/SpringbootShiroVueBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.winson.one;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootShiroVueBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
