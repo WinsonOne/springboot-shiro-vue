c08607e98ec545ef696a8bc7210cb86f12c5d7da
wenwc <wenwc@xiaopeng.com>
2019-09-27 14:07:38 +0800

    提交mybatis、pagehelper依赖

--- springboot-shiro-vue-backend/src/main/java/xyz/winson/one/SpringbootShiroVueBackendApplication.java
+++ springboot-shiro-vue-backend/src/main/java/xyz/winson/one/SpringbootShiroVueBackendApplication.java
@@ -1,5 +1,6 @@
 package xyz.winson.one;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019年09月25日 19:10
  */
 @SpringBootApplication
+@MapperScan("xyz.winson.one.mapper")
 public class SpringbootShiroVueBackendApplication {
 
 	public static void main(String[] args) {
